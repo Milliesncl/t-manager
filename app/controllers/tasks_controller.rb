bd9158274c1bb1862dd57d485f4708cce3c35957
Millie Senecal <millie.senecal@gmail.com>
2021-01-19 11:39:45 -0500

    views and links

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,32 @@
 class TasksController < ApplicationController
+    def index
+        @tasks = Task.all
+    end
+
+   def new
+        @task = Task.new
+   end
+
+   def create
+        @task = Task.new(task_params)
+        if @task.save
+            redirect_to list_path
+        else
+            render :new
+        end
+   end
+
+   def edit
+   end
+
+   def update
+   end
+
+   def destroy
+   end
+
+   private
+   def task_params
+    params.require(:task).permit(:title, :details, :completed)
+   end
 end
